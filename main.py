9daf2c925dfbe0ba044a609496e0c929a7773f6f
Ethan <ethan.ojaghi@outlook.com>
2026-04-27 21:01:14 +0100

    Comment out HF_HUB_OFFLINE and TRANSFORMERS_OFFLINE
    Comment out environment variable settings for offline mode.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import time 
 
-os.environ["HF_HUB_OFFLINE"] = "1"
-os.environ["TRANSFORMERS_OFFLINE"] = "1"
+# os.environ["HF_HUB_OFFLINE"] = "1"
+# os.environ["TRANSFORMERS_OFFLINE"] = "1"
 os.environ["HF_HUB_DISABLE_TELEMETRY"] = "1"
 os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
 
