ea1c56aac9ccf06a15046050090b4d6b2eca236d
Ethan <ethan.ojaghi@outlook.com>
2026-04-27 20:52:18 +0100

    Refactor Translator class and update translation method

--- translate.py
+++ translate.py
@@ -1,13 +1,10 @@
 import torch
-torch.set_num_threads(2)
-
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 
 class Translator:
-    def __init__(self, model_name="facebook/nllb-200-distilled-600M", target_lang=None):
+    def __init__(self, model_name):
         self.model_name = model_name
-        self.target_lang = target_lang
 
         self.tokenizer = AutoTokenizer.from_pretrained(
             self.model_name,
@@ -19,37 +16,23 @@ class Translator:
             local_files_only=True
         )
 
-        self.model.config.use_cache = True
-        self.model.generation_config.use_cache = True
-
-        self.tokenizer.src_lang = "eng_Latn"
+        self.model.eval()
 
         self.device = torch.device("cpu")
         self.model.to(self.device)
-        self.model.eval()
 
-    def translate(self, text, target_lang=None):
-        if target_lang is None:
-            target_lang = self.target_lang
-
+    def translate(self, text):
         text = text.strip()[:80]
 
         inputs = self.tokenizer(text, return_tensors="pt")
         inputs = {k: v.to(self.device) for k, v in inputs.items()}
 
-        forced_id = self.tokenizer.convert_tokens_to_ids(target_lang)
-
         with torch.inference_mode():
             outputs = self.model.generate(
-                input_ids=inputs["input_ids"],
-                attention_mask=inputs["attention_mask"],
-                forced_bos_token_id=forced_id,
-
+                **inputs,
                 num_beams=1,
                 do_sample=False,
-
-                max_new_tokens=10, 
+                max_new_tokens=40
             )
-        
 
-        return self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
